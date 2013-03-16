43646a3b0c48b7c4b4b288c9e4155f88339047f2
plbabin <plbabin@gmail.com>
2013-03-16 15:02:30 +0000

    Header custom avec le logo de l'opencode. Background générique sur l'app. Ajout d'un root controller

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,8 +1,43 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.makeKeyAndVisible
+
+    @window.backgroundColor = UIColor.blackColor
+
+    @viewController = MainController.alloc.init
+    @navigationController = UINavigationController.alloc.initWithRootViewController(@viewController)
+    
+    self.applyStylesheet
+
+    @window.rootViewController = @navigationController
 
     true
   end
 
-  
+  def applyStylesheet
+    UIApplication.sharedApplication.setStatusBarStyle(UIStatusBarStyleBlackOpaque)
+
+    navigationBar = UINavigationBar.appearance
+    navigationBar.tintColor = UIColor.blackColor
+
+    # remove default drop shadow on the header
+    navigationBar.setShadowImage(UIImage.alloc.init.autorelease)
+
+    img = UIImage.imageNamed("bg.jpg")
+    imgView = UIImageView.alloc.initWithFrame([[0,44],UIScreen.mainScreen.bounds.size])
+    imgView.contentMode = UIViewContentModeScaleAspectFill
+    imgView.clipsToBounds = true
+    imgView.image = img    
+    @navigationController.view.addSubview(imgView)
+    @navigationController.view.sendSubviewToBack(imgView)
+
+    # add opencode logo
+    w = UIScreen.mainScreen.bounds.size.width
+    img = UIImage.imageNamed("logo.png")
+    imgView = UIImageView.alloc.initWithFrame([[(w-125)/2,27],[125,30]])
+    imgView.image = img
+    @navigationController.view.addSubview(imgView)
+  end
 end
